9cf2f224e59a8d62c6985b6e66597fa1196c330b
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-09-23 23:16:20 -0500

    add js files

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,3 +1,8 @@
+import cart from './cart.js';
+import utilities from '../helpers/utilities.js';
+
+// pulled in cart from cart.js to use in this page
+
 const bookInfo = {
   price: 24.99,
   title: "The 50th Law",
@@ -5,16 +10,10 @@ const bookInfo = {
 };
 
 
-const printToDom = (divId, toPrint) => {
-  const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML = toPrint;
-};
-
-
 const addToCartEvent = () => {
-  console.log ('added to cart');
+  cart.setCart(bookInfo);
+  cart.cartToDom();
 };
-
 
 const makeStore = () => {
 let domString = '<h2> Our only book:</h2>';
@@ -22,7 +21,7 @@ domString += '<p>Buy it now:</p>';
 domString += `<h3>$${bookInfo.price}</h3>`;
 domString += `<img src=${bookInfo.image} alt="book-cover"/>`;
 domString += '<button  id="cart-button" type="button" class="btn btn-success col-12">Add to Cart</button>'; 
-printToDom('store-container', domString);
+utilities.printToDom('store-container', domString);
 document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
---
+++ javascripts/helpers/utilities.js
@@ -0,0 +1,8 @@
+const printToDom = (divId, toPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = toPrint;
+};
+
+
+
+export default {printToDom};
